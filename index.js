b82167021dde7220ef0293e183529b826107e520
Gustavo Rajab <122064946+gustajab@users.noreply.github.com>
2023-05-03 11:45:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 const express = require("express")
 const bodyParser = require("body-parser")
 const moment = require('moment');
-let ID = 1
+
+let ID  = 1
+
     let cursos = [
       { id: 0, nome: 'Curso de Node.js' },
     ];
     let alunos = [
-        { id: 0, nome: 'Gustavo', curso: 0, data_nasc:'2004/08/17' },
+        { id: 0, nome: 'Gustavo', curso: 0, data_nasc:'2004-08-17' },
       ];
-
 const port = process.env.PORT
 const app = express()
 
@@ -59,7 +60,6 @@ app.post('/cursos', (req,res) => {
       return curso;
     });
     const curso = cursos.find(c => c.id == id);
-    res.json({nome})
     res.send(`Curso ${curso.nome} atualizado com sucesso!`);
 })
 
@@ -75,45 +75,53 @@ app.post('/cursos', (req,res) => {
 
 // lista todos os alunos
 app.get('/alunos', (req,res) => {
+
+    
+
     const alunosComNomeCurso = alunos.map(a => {
         const curso = cursos.find(c => c.id === a.curso);
         const nomeCurso = curso ? curso.nome : 'Curso não encontrado';
         return { ...a, nomeCurso };
       });
       res.json(alunosComNomeCurso);
-    res.json(alunos)
+  
     
 })
   
   // adiciona um aluno
-app.post('/cursos', (req,res) => {
-    const {nome} = req.body
-    const {curso} = req.body
-    const {data_nasc} = req.body
-    cursos.push({id: ID++, nome, curso,data_nasc})
+app.post('/alunos', (req,res) => {
+
+    const {nome, curso, data_nasc} = req.body
+    alunos.push({id: ID++, nome, curso,data_nasc})
+
     res.json({message: 'OK'})
 })
   
-  // altera um curso
-  app.post('/cursos/alterar/:id', (req, res) => {
-    const { nome } = req.body;
+  // altera um aluno
+
+app.post('/alunos/alterar/:id', (req, res) => {
+    const { nome, curso, data_nasc } = req.body;
     const { id } = req.params;
-    cursos = cursos.map(curso => {
-      if (curso.id == id) {
-        curso.nome = nome;
+  
+    alunos = alunos.map(aluno => {
+      if (aluno.id == id) {
+        aluno.nome = nome;
+        aluno.curso = curso;
+        aluno.data_nasc = data_nasc;
       }
-      return curso;
+      return aluno;
     });
-    const curso = cursos.find(c => c.id == id);
-    res.json({nome})
-    res.send(`Curso ${curso.nome} atualizado com sucesso!`);
-})
+  
+    const aluno = alunos.find(a => a.id == id);
+    res.send(`Aluno ${aluno.nome} atualizado com sucesso!`);
+  });
+  
 
 
-  // remove um curso
-  app.delete('/cursos/:id', (req,res) => {
+// remove um aluno
+app.delete('/alunos/:id', (req,res) => {
     const {id} = req.params
-    const curso = cursos.find(c => c.id == id);
-    cursos = cursos.filter(c => c.id != id)
-    res.send(`Curso ${curso.nome} removido com sucesso!`);
-})
+    const aluno = alunos.find(a => a.id == id);
+    alunos = alunos.filter(a => a.id != id)
+    res.send(`Aluno ${aluno.nome} removido com sucesso!`);
+  })
